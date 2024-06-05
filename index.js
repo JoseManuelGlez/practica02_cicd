7e5136f11c6b890bb11204d4826b680de7724a7a
Jose Manuel galindo gonzalez <213505@ids.upchiapas.edu.mx>
2024-06-05 13:59:29 -0600

    saber cual commit es3

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/api/v1/welcome', (req, res) => {
-    res.send('holu, Mundos!');
+    res.send('hu, Mundos!');
 });
 
 app.listen(port, () => {
