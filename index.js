64b9c5ae9b02ffcacadc800ba68ed6beed05295a
Jose Manuel galindo gonzalez <213505@ids.upchiapas.edu.mx>
2024-06-07 00:04:42 -0600

    jenkins file28

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/api/v1/welcome', (req, res) => {
-    res.send('hu, Mundos x2025!');
+    res.send('hu, Mundos x2026!');
 });
 
 app.listen(port, () => {
